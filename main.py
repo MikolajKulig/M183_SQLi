f11ca1626f57cacd64b0d230c1a8f41636d0b90e
MikolajKulig <mikolaj.kulig08@gmail.com>
2025-10-26 17:09:48 +0100

    Added database connection

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    pass
+from fastapi import FastAPI
+from db import get_users
 
+app = FastAPI()
 
-if __name__ == '__main__':
-    main()
+@app.get("/test")
+def helloWorld():
+    return "Hello world"
+
+@app.get("/users")
+def getUsers():
+    return get_users()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
